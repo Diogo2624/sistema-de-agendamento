b99127041c5d36a8dc3b62fe0f502e4af39f1921
Diogo Henrique <henriquepanazzolo2018@hotmail.com>
2025-10-30 14:01:07 -0300

    Criando o Modal

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botoes = document.querySelectorAll('.btn-agendar');
+    const modal = document.getElementById('modal-agendar');
+    const fechar = modal.querySelector('.fechar');
+    const escolher = document.getElementById('btn-escolher-profissional');
+    const profissionais = document.getElementById('profissionais');
+
+    botoes.forEach(botao => {
+        botao.addEventListener('click', () => {
+            modal.classList.add("ativo");
+            document.body.style.overflow = "hidden";
+        });
+    });
+
+    fechar.addEventListener("click", fecharModal);
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) fecharModal();
+    });
+
+    function fecharModal() {
+        modal.classList.remove("ativo");
+        document.body.style.overflow = "auto";
+    }
+
+    escolher.addEventListener("click", () => {
+        fecharModal();
+        setTimeout(() => {
+            profissionais.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }, 300);
+    });
+});
